6c8dc8ca466a275148cd87c7b80e515dbf3ab3f6
Saad Ahmad <saad415415@gmail.com>
2025-04-27 14:00:32 +0200

    App icon and name changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,8 @@ android {
         applicationId = "com.example.mosque"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
+        versionCode = 2
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // Adding BuildConfig fields
         buildConfigField("String", "ADMIN_USER", "\"superuser\"")
